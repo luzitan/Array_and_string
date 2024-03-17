314529554eb5da85572ec7e2a7c58975949f5efd
Vera Bylinskaia <iagyar@list.ru>
2024-03-17 03:08:13 +0300

    Задача 2

--- Program.cs
+++ Program.cs
@@ -1,13 +1,20 @@
 ﻿// Задача 1: Задайте двумерный массив символов (тип char [,]).
 // Создать строку из символов этого массива.
 
-Console.Clear();
+// Console.Clear();
 
-char [,] array = {{'1', '2', '3'}, {'4', '5', '6'}};
-string str = "";
-for (int i = 0; i < array.GetLength(0); i++){
-    for (int j = 0; j < array.GetLength(1); j++){
-        str += array[i,j];
-    }
-}
-Console.WriteLine(str);
+// char [,] array = {{'1', '2', '3'}, {'4', '5', '6'}};
+// string str = "";
+// for (int i = 0; i < array.GetLength(0); i++){
+//     for (int j = 0; j < array.GetLength(1); j++){
+//         str += array[i,j];
+//     }
+// }
+// Console.WriteLine(str);
+
+
+// Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах.
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные. 
+
+string str = "LuZiTANo";
+Console.Write(str.ToLower());

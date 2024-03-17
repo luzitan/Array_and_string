a2c55f68db0c344e13cd555a5c184980bec68e58
Vera Bylinskaia <iagyar@list.ru>
2024-03-17 03:05:37 +0300

    Задача 1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 1: Задайте двумерный массив символов (тип char [,]).
+// Создать строку из символов этого массива.
+
+Console.Clear();
+
+char [,] array = {{'1', '2', '3'}, {'4', '5', '6'}};
+string str = "";
+for (int i = 0; i < array.GetLength(0); i++){
+    for (int j = 0; j < array.GetLength(1); j++){
+        str += array[i,j];
+    }
+}
+Console.WriteLine(str);
